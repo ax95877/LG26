f8a7c12b806b5bf35b17d2ded0d679375ae4839f
ax95877 <ax95877@gmail.com>
2025-09-22 14:28:21 -0600

    Initial Commit

---
+++ LG26/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LG26
+//
+//  Created by Alex on 2025-09-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LG26/LG26App.swift
@@ -0,0 +1,17 @@
+//
+//  LG26App.swift
+//  LG26
+//
+//  Created by Alex on 2025-09-22.
+//
+
+import SwiftUI
+
+@main
+struct LG26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
